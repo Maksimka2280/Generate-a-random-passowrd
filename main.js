3cb913ed549c1aad9f3f58f02d275a815d6b4338
Maksimka2280 <maksimkalol1402@gmail.com>
2024-05-22 10:46:07 +0300

    пофиксил баг с редактором размера пароля

--- main.js
+++ main.js
@@ -6,17 +6,10 @@ const arr = "qwertyuiopasdfghjklzxcvbnm1234567890".split("");
 
 
 button.addEventListener('click', () => {
-    maRange.addEventListener('change', () => {
-        let length = Math.min(maRange.value, 32);
+   let length = Math.min(maRange.value, 32);
         let password = randomizer(length);
         output.innerHTML = password;
-       
-      
-    })
-
 });
-
-
 deleteButton.addEventListener('click', () => {
     output.innerHTML = '';
 });
@@ -24,7 +17,6 @@ function randomizer(length) {
     let password = '';
     for (let i = 0; i < length; i++) {
         password += arr[Math.floor(Math.random() * arr.length )];
-
     }
     return password;
 }
